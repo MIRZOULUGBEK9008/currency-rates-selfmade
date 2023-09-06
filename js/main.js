3e8d067f8161202635d1f8a93af713f1985b5aed
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2023-09-06 17:59:33 +0500

    Add api, apiKey and flagSource

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
-const elLoader = document.querySelector('.js-loader');
-
+// ? Loader
 document.addEventListener('DOMContentLoaded', () => {
+  const elLoader = document.querySelector('.js-loader');
   setTimeout(() => {
     elLoader.classList.add('loader-wrapper--none');
   }, 800);
 });
+
+// Variables
+const apiKey = "aa1a46ce5318650594f4612d";
+const api = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
+const flagsOfCountriesSource = `https://hatscripts.github.io/circle-flags/flags/${countryCode}.svg`;
